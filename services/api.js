662cdf9ab7b52fcd70362d62e5bcf31c69be29c8
Júlia <Julia@MacBook-Pro-de-Alisson.local>
2025-11-12 16:35:13 -0300

    atualizacao

--- services/api.js
+++ services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api-sipdus-g4cuyl68s-giovanas-projects-936114ef.vercel.app';
+const BASE_URL = 'https://api-sipdus-hlr4dok0j-giovanas-projects-936114ef.vercel.app';
 const TIMEOUT = 5000;
 
 const api = {
